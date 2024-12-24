4db00fcbcc9820cf6e7e61487ee813d12660cda2
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2024-12-24 10:06:44 +0500

    fully done

--- main.js
+++ main.js
@@ -1,63 +1,84 @@
+// const myPromise = new Promise((res, rej) => {
+//   const success = true;
+//   if (success) {
+//     res("success");
+//   } else {
+//     rej("failed");
+//   }
+// });
+
 const mycard = new XMLHttpRequest();
 mycard.open("GET", "https://fakestoreapi.com/products");
 mycard.send();
-mycard.addEventListener("readystatechange", () => {
-  if (mycard.readyState === 4 && mycard.status == 200) {
-    getdata(JSON.parse(mycard.responseText));
-  }
-});
-
-function getdata(data) {
-  const cards = document.querySelector("cards");
+function getdata() {
+  return new Promise((res, rej) => {
+    mycard.addEventListener("readystatechange", () => {
+      if (mycard.readyState === 4 && mycard.status == 200) {
+        res(JSON.parse(mycard.responseText));
+      } else if (mycard.readyState === 4) {
+        rej(mycard.responseText);
+      }
+    });
+  });
+}
+getdata()
+  .then((data) => {
+    getdataFunc(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+function getdataFunc(data) {
+  const cards = document.querySelector(".cards");
 
   data.forEach((element) => {
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = `
-          <div class="relative">
-            <img
-              class="w-[80%] mx-auto image"
-              src="${element.image}"
-              alt="" />
-            <p
-              class="category p-2 bg-[#5b5959] rounded-[10px] text-white w-fit text-[12px] absolute top-1">
-              ${element.category}
-            </p>
-          </div>
-          <div class="textarea">
-            <div class="text-ellipsis">
-              <p class="font-bold">
-              ${element.title}
+            <div class="relative">
+              <img
+                class="w-[80%] mx-auto image"
+                src="${element.image}"
+                alt="" />
+              <p
+                class="category p-2 bg-[#5b5959] rounded-[10px] text-white w-fit text-[12px] absolute top-1">
+                ${element.category}
               </p>
             </div>
-            <div>
-              <p class="text-[14px] py-2 card-text multi-line">
-              ${element.description}
-              </p>
+            <div class="textarea">
+              <div class="text-ellipsis">
+                <p class="font-bold">
+                ${element.title}
+                </p>
+              </div>
+              <div>
+                <p class="text-[14px] py-2 card-text multi-line">
+                ${element.description}
+                </p>
+              </div>
+              <div>
+                <div class="flex items-center gap-3">
+                  <p class="font-bold">rank:</p>
+                  <p>${element.rating.rate}</p>
+                </div>
+                <div class="flex items-center gap-3">
+                  <p class="font-bold">rank:</p>
+                  <p>${element.price} $</p>
+                </div>
+  
+                <div class="flex items-center gap-3">
+                  <p class="font-bold">have in store:</p>
+                  <p>${element.rating.count}</p>
+                </div>
+              </div>
             </div>
-            <div>
-              <div class="flex items-center gap-3">
-                <p class="font-bold">rank:</p>
-                <p>${element.rating.rate}</p>
-              </div>
-              <div class="flex items-center gap-3">
-                <p class="font-bold">rank:</p>
-                <p>${element.price} $</p>
-              </div>
-
-              <div class="flex items-center gap-3">
-                <p class="font-bold">have in store:</p>
-                <p>${element.rating.count}</p>
-              </div>
+            <div class="flex items-center justify-between gap-2">
+              <button
+                class="p-3 rounded-[10px] w-[100%] border border-black hover:bg-black hover:text-white active:scale-[0.9] transition-all">
+                savatga
+              </button>
             </div>
-          </div>
-          <div class="flex items-center justify-between gap-2">
-            <button
-              class="p-3 rounded-[10px] w-[100%] border border-black hover:bg-black hover:text-white active:scale-[0.9] transition-all">
-              savatga
-            </button>
-          </div>
-        `;
-    document.body.appendChild(card);
+          `;
+    cards.appendChild(card);
   });
 }
